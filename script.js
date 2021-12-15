041649c61395427012f2aa51bddeb2f9d51be0b7
MatVicDev <matheus@teste.com>
2021-12-15 15:14:16 -0300

    Efetuando o cálculo e apresntando na tela

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function calcular(tipo, valor) {
-	console.log(tipo, valor)
+	if(tipo === 'acao') {
+
+		if(valor === 'c') {
+			document.getElementById('resultado').value = ''
+		}
+
+		if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+			document.getElementById('resultado').value += valor
+		}
+
+		if(valor === '=') {
+			var resultado = eval(document.getElementById('resultado').value)
+
+			document.getElementById('resultado').value = resultado
+		}
+
+	} else if(tipo === 'valor') {
+		document.getElementById('resultado').value += valor
+	}
 }
